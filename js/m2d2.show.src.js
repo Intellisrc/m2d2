b3aeea47f752ee9fed6f651a04a9aae4158eb9ba
A. Lepe <dev@alepe.com>
2018-06-05 13:48:31 +0900

    Added explanation in extensions

--- js/m2d2.show.src.js
+++ js/m2d2.show.src.js
@@ -1,3 +1,10 @@
+/**
+ * @author: A. Lepe
+ * @url : https://gitlab.com/lepe/m2d2/
+ * @since: May, 2018
+ * 
+ * This is an extension to use the property "show" to hide/show elements
+ */
 m2d2.ext({
 	show : function(show, $node) { if(show) { $node.show(); } else { $node.hide();} }
 });
--- js/m2d2.style.src.js
+++ js/m2d2.style.src.js
@@ -1,3 +1,10 @@
+/**
+ * @author: A. Lepe
+ * @url : https://gitlab.com/lepe/m2d2/
+ * @since: May, 2018
+ * 
+ * This is an extension to use propertes to set style in elements
+ */
 m2d2.ext({
 	color : function(value) { return { style : "color:"+value }; },
 	css	  : function(value) { return { 'class' : value }; }
