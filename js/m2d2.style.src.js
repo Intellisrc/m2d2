abf672cb50b7d7b37af1f178a9d2315f8eece02f
A. Lepe <dev@alepe.com>
2018-08-13 16:59:01 +0900

    Added support for combined styles

--- js/m2d2.style.src.js
+++ js/m2d2.style.src.js
@@ -6,7 +6,7 @@
  * This is an extension to use propertes to set style in elements
  */
 m2d2.ext({
-	color : function(value) { return { style : "color:"+value }; },
-	bgcolor : function(value) { return { style : "background-color:"+value }; },
-	css	  : function(value) { return { 'class' : value }; }
+	color   : function(value, elem) { this.style.color = value; },
+	bgcolor : function(value, elem) { this.style.backgroundColor = value; },
+	css	    : function(value)       { return { 'class' : value }; }
 });
