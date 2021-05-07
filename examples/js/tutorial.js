dd3fe54d164f8911f308aa19e606d4244033c8e3
A. Lepe <dev@alepe.com>
2021-05-07 10:54:50 +0900

    v.2.0.0 : Changed tutorial to use codepen instead.

--- examples/js/tutorial.js
+++ examples/js/tutorial.js
@@ -1,5 +1,17 @@
 m2d2.ready($ => {
     const body = $("body");
-    const nav = $.newNode("nav");
-    body.appendChild(nav);
+    const ol = $.newNode("ol");
+    body.appendChild(ol);
+    const nav = $(ol, {
+        css : "nav",
+        template : { li : { a : {} }},
+        items : [
+            { a : { text : "How to start...", href : "index.html" } },
+            { a : { text : "DOM objects", href : "dom-objects.html" } },
+        ]
+    });
+    body.findAll('code').forEach(block => {
+        block.classList.add("js");
+        hljs.highlightElement(block);
+    });
 });
