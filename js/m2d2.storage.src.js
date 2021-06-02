110f4f1406319e3d67cd6c9ff9b377852a0bc24f
A. Lepe <dev@alepe.com>
2021-06-02 10:42:51 +0900

    v.2.0.0 : Updated comments in storage extension

--- js/m2d2.storage.src.js
+++ js/m2d2.storage.src.js
@@ -15,15 +15,23 @@ m2d2.load($ => {
      * @param method: local, session (which means:)
      *                localStorage, sessionStorage (default)
      * Methods:
-     * set : saves the information
-     * get : retrieve the info.
-     * del : remove data
-     * log : add in array (it will keep "n" number of items in queue)
+     * set      : saves the information
+     * get      : retrieve the info.
+     * del      : remove data
+     * clear    : remove all keys
+     * exists   : check if key exists
+     * log      : add in array (it will keep "n" number of items in queue)
      *
      * The way to use it is:
-     * var local = new Storage('local');
-     * local.set('mykey','myval');
-     * alert(local.get('mykey'));
+     *
+     * $.local.set("mykey",myval);
+     * console.log(local.get("mykey"));
+     * console.log(local.exists("mykey")); // returns: true
+     * $.local.del("mykey");
+     * $.local.log("mylog", message, 10); // Keep only the last 10
+     * $.local.clear(); // Remove all keys
+     *
+     * myval can be : string, number, object, array
      */
     function Storage(method) {
         switch(method) {
@@ -32,13 +40,13 @@ m2d2.load($ => {
         }
         if(this.method == undefined) this.method = localStorage; //Default
         this.set = function(key, val) { if(typeof(val) == 'string') val = { '$' : val }; this.method.setItem(key, JSON.stringify(val)); }
-        this.get = function(key) { var val = JSON.parse(this.method.getItem(key)) || {}; if(val["$"] != undefined) val = val["$"]; else if(Object.keys(val).length === 0 && val.constructor === Object) val = null; return val; }
+        this.get = function(key) { let val = JSON.parse(this.method.getItem(key)) || {}; if(val["$"] != undefined) val = val["$"]; else if(Object.keys(val).length === 0 && val.constructor === Object) val = null; return val; }
         this.del = function(key) { this.method.removeItem(key); }
         this.clear = function() { this.method.clear(); }
         this.exists = function(key) { return this.method.hasOwnProperty(key); }
         this.log = function(key, val, n) {
             if(n == undefined) n = 10;
-            var tmp = this.get(key) || [];
+            const tmp = this.get(key) || [];
             tmp.push(val);
             while(tmp.length > n) tmp.shift();
             this.set(key,tmp);
