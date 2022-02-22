61014f7cb8206f0e8496547fddd72daa5ff96c81
A.Lepe <lepe@support.ne.jp>
2022-02-22 13:34:10 +0900

    2.1.1 Working with documentation

--- test/02_test_css_class.js
+++ test/02_test_css_class.js
@@ -8,3 +8,21 @@ QUnit.test('adding class using array', function (assert) {
   assert.ok(q.classList.contains('foo'));
   assert.ok(q.classList.contains('boo'));
 })
+
+QUnit.test('adding or removing classes using object', function (assert) {
+  $(root, `
+  <div id="main" class="one two three"></div>
+  `);
+   //-------
+  const d = $("#main", {
+    css :  {
+        two  : false,
+        four : true,
+        five : true
+    }
+  });
+  assert.equal(d.css.length, 4);
+  assert.ok(! d.css.contains('two'));
+  assert.ok(d.css.contains('four'));
+  assert.ok(d.css.contains('five'));
+})
